c36b5e7e9af1af77953f460ddc0f4af0666e7f81
Stan <stanstanov@gmail.com>
2026-08-19 17:10:26 +0200

    save

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "lt.turron.sivelo"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 4
         versionName = "1.0"
     }
 
